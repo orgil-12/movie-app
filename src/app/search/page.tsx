8fa92d2340e2293a7bbf5fff9f32aafac2a4a86b
orgil-12 <theorgilbold@gmail.com>
2025-01-10 15:40:59 +0800

    init

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -7,8 +7,8 @@ import { PaginationComp } from "../_components/Pagination";
 import MovieCard from "../_components/MovieCard";
 import { SearchGenre } from "../_components/SearchGenre";
 
+const Loading = () => (<div>Loading</div>);
 
-export const Loading = ()=> (<div>Loading</div>)
 export default function Page() {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [pageInfo, setPageInfo] = useState<PageInfo>({
@@ -19,6 +19,7 @@ export default function Page() {
   const searchParams = useSearchParams();
   const query = searchParams.get("query");
   const page = searchParams.get("page");
+
   useEffect(() => {
     async function fetchGenreMovies() {
       const response = await fetch(
@@ -35,28 +36,30 @@ export default function Page() {
     }
     fetchGenreMovies();
   }, [query, page]);
-  if (!movies) {
-    return <Loading />
+
+  if (!movies.length) { // Changes from `!movies` to `!movies.length`
+    return <Loading />;
   }
+
   return (
-<Suspense fallback={<Loading />}>
-<div className="flex flex-col pt-8 gap-8 xl:grid xl:grid-cols-[auto,auto,auto] px-5 py-8 sm:px-[40px] md:px-[60px] lg:px-[80px] mt-14">
-      <h1 className="font-semibold text-2xl xl:col-span-3 ">Search results</h1>
-      <div className="flex flex-col gap-5 xl:col-span-2 xl:border-r xl:pr-8  ">
-        <h1 className="text-xl font-bold  ">
-          {totalResults} results for "{query}"
-        </h1>
-        <div className="gap-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 ">
-          {movies?.map((movie) => (
-            <span key={"movie" + movie.id}>
-              <MovieCard movie={movie} />
-            </span>
-          ))}
+    <Suspense fallback={<Loading />}>
+      <div className="flex flex-col pt-8 gap-8 xl:grid xl:grid-cols-[auto,auto,auto] px-5 py-8 sm:px-[40px] md:px-[60px] lg:px-[80px] mt-14">
+        <h1 className="font-semibold text-2xl xl:col-span-3 ">Search results</h1>
+        <div className="flex flex-col gap-5 xl:col-span-2 xl:border-r xl:pr-8">
+          <h1 className="text-xl font-bold">
+            {totalResults} results for "{query}"
+          </h1>
+          <div className="gap-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4">
+            {movies.map((movie) => (
+              <span key={"movie" + movie.id}>
+                <MovieCard movie={movie} />
+              </span>
+            ))}
+          </div>
+          {pageInfo.totalPage > 1 && <PaginationComp pageInfo={pageInfo} />}
         </div>
-      {pageInfo.totalPage > 1 && <PaginationComp pageInfo={pageInfo} />}
+        <SearchGenre />
       </div>
-      <SearchGenre />
-    </div>
-</Suspense>
+    </Suspense>
   );
 }
